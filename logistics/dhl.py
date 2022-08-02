b0a017efd5ebae7b487aa9877dee32e21a30cedc
Kim Yangsu <y.kim@snu.ac.kr>
2022-08-02 22:02:50 +0900

    Parce and print more information

--- logistics/dhl.py
+++ logistics/dhl.py
@@ -33,10 +33,34 @@ class Logistics(BaseLogistics):
             'https://api-eu.dhl.com/track/shipments'
         )
 
+        # source: https://developer.dhl.com/sites/default/files/2022-07/dpdhl_tracking-unified_1.3.2.yaml
+        self.service = {
+            'dgf':       'DHL Global Forwarding',  # DGF Global Forwarding
+            'sameday':   'DGF SameDay',            # DGF SameDay
+            'dsc':       'DHL Supply Chain',       # DSC (Connected View)
+            'express':   'DHL Express',            # Express (XMLPI-ITS)
+            'freight':   'DHL Freight',            # Freight (Active Tracing)
+            'ecommerce': 'DHL eCS (US)',           # eCS (Americas/WebTrack)
+            'ecommerce-apac': 'DHL eCS(APAC)',
+            'ecommerce-europe': 'DHL eCS(EU)',
+            'parcel-de': 'DHL eCS (DE)',           # eCS DE (Parcel DE/NOLP)
+            'parcel-nl': 'DHL eCS (NL)',           # eCS NL (Parcel BNL/PDS)
+            'parcel-pl': 'DHL eCS (PL)',           # eCS PL (Parcel PL/TNT)
+            'parcel-uk': 'DHL eCS (UK)',           # eCS UK (Parcel UK)
+            'post-de':   'Deutsche Post',          # Post Germany (Track Trace Brief)
+        }
+        self.status_code = {
+            'pre-transit': '배송준비',
+            'transit':     '배송중',
+            'delivered':   '완료',
+            'failure':     '실패',
+            'unknown':     '확인안됨',
+        }
+
     def process(self, trackNo, year):
         info = get_info_base(self.code, trackNo)
         if not self.config:
-            print('https://developer.dhl/ 등록 후 키를 발급 받으세요')
+            print('https://developer.dhl/ 에서 발급받은 키를 config.yaml에 적어주세요.')
             return info
 
         self.headers['DHL-API-Key'] = self.config['key']
@@ -45,14 +69,22 @@ class Logistics(BaseLogistics):
 
         if 'shipments' in self.root and len(self.root['shipments']) == 1:
             ship = self.root['shipments'][0]
-            info['info']['서비스'] = ship['service']
+            info['info']['서비스'] = self.service.get(ship['service'], ship['service'])
             info['info']['제품'] = ship['details']['product']['productName']
             info['info']['출발지'] = get_address(ship['origin'])
             info['info']['도착지'] = get_address(ship['destination'])
             info['info']['무게'] = '{value} {unitText}'.format(**ship['details']['weight'])
             info['info']['현위치'] = get_address(ship['status']['location'])
-            info['info']['상태'] = ship['status']['statusCode']
+            info['info']['상태'] = self.status_code.get(ship['status']['statusCode'], ship['status']['statusCode'])
             info['info']['시간'] = self.parse_time(ship['status']['timestamp'])
+            if 'estimatedTimeOfDelivery' in ship:
+                info['info']['예상배송일'] = self.parse_time(ship['estimatedTimeOfDelivery'])
+            elif 'estimatedDeliveryTimeFrame' in ship:
+                timeframe = ship['estimatedDeliveryTimeFrame']
+                info['info']['예상배송일'] = '{} - {}'.format(self.parse_time(timeframe['estimatedFrom']).strftime('%m/%d %H:%M'), self.parse_time(timeframe['estimatedThrough']).strftime('%m/%d %H:%M'))
+            if 'estimatedTimeOfDeliveryRemark' in ship:
+                info['info']['배송안내'] = ship['estimatedTimeOfDeliveryRemark']
+            info['info']['배달증명'] = '있음' if ship['details']['proofOfDeliverySignedAvailable'] else '없음'
 
             for event in reversed(ship['events']):
                 if 'location' in event:
